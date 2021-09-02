0091a571b6ce954b68e47283a40d900992d2c464
meetowl <mrontionov@meetowl.site>
2021-09-02 10:29:34 +0100

    Objectified the serial connection and actions

--- py/bot.py
+++ py/bot.py
@@ -12,13 +12,9 @@ ARDUINO_SERIAL_PORT = "/dev/ttyACM0"
 ARDUINO_SERIAL_SPEED = 9600
 
 class FresherBot(discord.Client):
-    def initialise(self, dataPath, serialCon):
+    def __init__(self, dataPath, fresherUno):
         self.dataPath = dataPath
-        self.serialCon = serialCon
-
-    def __init__(self, dataPath, serialCon):
-        self.dataPath = dataPath
-        self.serialCon = serialCon
+        self.fresherUno = fresherUno
         # run the constructor of our parent (discord.Client)
         super(FresherBot, self).__init__()
 
@@ -29,14 +25,19 @@ class FresherBot(discord.Client):
     async def on_message(self, message):
         if message.author.name == UserVars.DISCORD_USER:
             if message.content == 'newuser':
-                self.serialCon.write(b'Q')
-                self.serialCon.write(bytes(str(len('newuser')), 'ascii'))
+                self.fresherUno.discordBlink(len('newuser'))
 
 #    async def on_member_join(self, member):
-        
 
-        
+class FresherUno:
+    def __init__(self, serPort, serSpeed):
+        self.serialCon = serial.Serial(serPort, serSpeed, timeout=1)
+        # Wait for serial to initialise
+        sleep(3)
 
+    def discordBlink(self, times):
+        self.serialCon.write(b'Q')
+        self.serialCon.write(bytes(str(times), 'ascii'))
 
 
 def main():
@@ -51,9 +52,7 @@ def main():
         exit()
 
     try:
-        serialCon = serial.Serial(ARDUINO_SERIAL_PORT, ARDUINO_SERIAL_SPEED, timeout=1)
-        # Wait for serial to initialise
-        sleep(3)
+        fUno = FresherUno(ARDUINO_SERIAL_PORT, ARDUINO_SERIAL_SPEED)
     except serial.serialutil.SerialException as se:
         print('error: could not connect to serial: %s' % se)
         quit(1)
@@ -62,7 +61,7 @@ def main():
     if not os.path.isdir(path):
         os.makedirs(path)
 
-    discordBot = FresherBot(path, serialCon)
+    discordBot = FresherBot(path, fUno)
     discordBot.run(UserVars.DISCORD_API)
 
     
