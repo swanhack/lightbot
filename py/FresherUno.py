40e3ab81a8f41b35b75a37a0f8d78f3e635f12a5
meetowl <mrontionov@meetowl.site>
2021-09-09 10:05:30 +0100

    [Bot] Minor ambiguity change
    If you send bytes(N), where N is not a list but a single int, it sends
    N 0 bytes, rather than one byte of N. This clears that ambiguity.

--- py/FresherUno.py
+++ py/FresherUno.py
@@ -8,20 +8,20 @@ SSIG_SET_DEFAULT_COLOUR = 2
 
 class FresherUno:
         def __init__(self, serPort, serSpeed):
-                self.serialCon = serial.Serial(serPort, serSpeed, timeout=1)
+                self.serialCon = serial.Serial(serPort, serSpeed, timeout=15, write_timeout=15)
                 self.lock = threading.Lock()
                 # Wait for serial to initialise
                 sleep(3)
 
         def discordBlink(self, times, accentColour):
                 self.lock.acquire()
-                serialCommand = bytes([SSIG_DISCORD_JOIN, accentColour[0], accentColour[1],
+                serialCommand = bytearray([SSIG_DISCORD_JOIN, accentColour[0], accentColour[1],
                                        accentColour[2], times])
                 self.serialCon.write(serialCommand)
                 self.lock.release()
 
         def setDefaultColour(self, colour):
                 self.lock.acquire()
-                serialCommand = bytes([SSIG_SET_DEFAULT_COLOUR, colour[0], colour[1], colour[2]])
+                serialCommand = bytearray([SSIG_SET_DEFAULT_COLOUR, colour[0], colour[1], colour[2]])
                 self.serialCon.write(serialCommand)
                 self.lock.release()
