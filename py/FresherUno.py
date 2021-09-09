2f48243f4aeb181a71da7b9c132044fdb1118d24
meetowl <mrontionov@meetowl.site>
2021-09-09 10:07:15 +0100

    [Bot] Ability to query arduino for current strip colour

--- py/FresherUno.py
+++ py/FresherUno.py
@@ -5,7 +5,7 @@ import threading
 # Serial signals
 SSIG_DISCORD_JOIN = 1
 SSIG_SET_DEFAULT_COLOUR = 2
-
+SSIG_QUERY_STATE = 3
 class FresherUno:
         def __init__(self, serPort, serSpeed):
                 self.serialCon = serial.Serial(serPort, serSpeed, timeout=15, write_timeout=15)
@@ -25,3 +25,16 @@ class FresherUno:
                 serialCommand = bytearray([SSIG_SET_DEFAULT_COLOUR, colour[0], colour[1], colour[2]])
                 self.serialCon.write(serialCommand)
                 self.lock.release()
+
+        def queryCurrentColour(self):
+                self.lock.acquire()
+                self.serialCon.write(bytearray([SSIG_QUERY_STATE]))
+                RGB_BYTE_SIZE = 3
+                currentStateBytes = self.serialCon.read(RGB_BYTE_SIZE)
+                self.lock.release()
+                if not len(currentStateBytes) == RGB_BYTE_SIZE:
+                        raise RuntimeError("didn't read full rgb state string from arduino")
+                currentState = (currentStateBytes[0], currentStateBytes[1],
+                                currentStateBytes[2])
+                return currentState
+                     
