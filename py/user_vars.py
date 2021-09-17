50a71099677479ebfb6fff2f5379bf9828dd42eb
meetowl <mrontionov@meetowl.site>
2021-09-17 12:01:52 +0100

    Added bot channel user variable

--- py/user_vars.py
+++ py/user_vars.py
@@ -2,10 +2,12 @@
 # WARNING:    Due to the sensitive nature of this file, make sure it is not tracked by git
 # WARNING:    You can tell git to assume it is unchanged by running:
 # WARNING:        git update-index --assume-unchanged py/user_vars.py 
-# WARNING:    You can revert this to update the file with
+# WARNING:    You can revert this (to update the file) with
 # WARNING:        git update-index --no-assume-unchanged py/user_vars.py 
 class UserVars():
     # Fill this with your Discord bot's API key
     DISCORD_API = ''
     # Fill this with your name for "privileged mode"
     DISCORD_USER = ''
+    # ID of channel the bot should respond to
+    DISCORD_BOT_MSG_CHANNEL = ''
